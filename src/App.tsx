85a4089c9fea5c35ef4451713430ed7c14af7c16
Alioune Badara <mamesaliousante@gmail.com>
2020-10-13 06:58:55 +0000

    Setup our App Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React,{FunctionComponent,useState,useEffect} from "react";
-import PokemonList from "./pages/pokemon-list"
+import React,{FunctionComponent } from "react";
+import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 const App: FunctionComponent = () => {
     
    
     return (
-     <PokemonList />
+     <Router>
+         <div>
+            {/* navigator bar of all our Application */}
+            <nav>
+                <div className="nav-wrapper teal">
+                <Link to="/" className="brand-logo center">Pokedex</Link>
+                </div>
+            </nav>
+            {/* Gestionary System of for all our App router */}
+            <Switch>
+                <Route exact path="/" component={PokemonList} />
+                <Route exact path="/pokemons" component={PokemonList} />
+                <Route path="/pokemons/:id" component={PokemonsDetail} />
+            </Switch>
+        </div>
+     </Router>
     )
 }
 
--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -3,7 +3,7 @@ import Pokemon from "../models/pokemon";
 import "./pokemon-card.css"
 import formatDate from '../helpers/format-date';
 import formatType from '../helpers/format-type';
-
+import { useHistory } from 'react-router-dom';
 
 
 type Props = {
@@ -14,7 +14,7 @@ type Props = {
 const PokemonCard: FunctionComponent<Props> = ({pokemon,borderColor = '#009688'}) => {
 
     const [color,setColor] = useState<string>();
-   
+    const history = useHistory();
     const showBorder = () => {
         setColor(borderColor);
     }
@@ -23,11 +23,14 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon,borderColor = '#009688'}
         setColor('#f5f5f5'); // we make a grey border-color as default
     }
 
+    const gotoPokemon = (id: number) => {
+      history.push(`pokemons/${id}`)
+    }
    
 
 
     return (
-    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideColor}>
+    <div className="col s6 m4" onClick={() => gotoPokemon(pokemon.id)} onMouseEnter={showBorder} onMouseLeave={hideColor}>
       <div className="card horizontal" style={{borderColor: color}}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
