95245dc7db8842832dbd9ec052e0901f84180d0b
Alioune Badara <mamesaliousante@gmail.com>
2020-10-13 01:12:39 +0000

    App first setup

---
+++ README.md
@@ -0,0 +1,1 @@
+#MY POKEMON APP
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React,{FunctionComponent,useState,useEffect} from "react";
+import PokemonList from "./pages/pokemon-list"
+
+
+const App: FunctionComponent = () => {
+    
+   
+    return (
+     <PokemonList />
+    )
+}
+
+
+export default App;
+
+
+
+
+
+
+
