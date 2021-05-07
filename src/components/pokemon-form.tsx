4d073fdd8c935058f150a532b1ca3889fdb40f5b
Aliou Badara <alioubkeita95@gmail.com>
2021-05-07 07:17:40 +0000

    Modify a pokemon : setup and save new data to the db

--- src/components/pokemon-form.tsx
+++ src/components/pokemon-form.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent,useState } from 'react';
 import {useHistory} from 'react-router-dom';
 import Pokemon from '../models/pokemon';
 import formatType from '../helpers/format-type';
+import PokemonService from '../services/pokemon-services';
 
   
 type Props = {
@@ -75,7 +76,11 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
     const isFormValid = validateForm();
 
     if(isFormValid) {
-      history.push(`/pokemons/${pokemon.id}`)
+      pokemon.name =form.name.value;
+      pokemon.hp =form.hp.value;
+      pokemon.cp =form.cp.value;
+      pokemon.types =form.types.value;
+      PokemonService.updatePokemon(pokemon).then(() => history.push(`/pokemons/${pokemon.id}`));
     }
   }
 
