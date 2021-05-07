dc38ca1afa785c3fa90a01a75dd152199a6e9b14
Aliou Badara <alioubkeita95@gmail.com>
2021-05-07 07:47:03 +0000

    Delete a pokemon: setup and save new data in db

--- src/services/pokemon-services.ts
+++ src/services/pokemon-services.ts
@@ -27,7 +27,17 @@ export default class PokemonService {
     .then(respone => respone.json())
     .catch(error => this.handleError(error))
   }
- 
+  
+  static deletePokemon(pokemon: Pokemon): Promise<{}> {
+    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+      method: 'DELETE',
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .catch(error => this.handleError(error));
+  }
+
+
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
   }
