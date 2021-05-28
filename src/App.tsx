4306e088110c910a7fd90678ffa37e263ee27a95
Aliou Badara <alioubkeita95@gmail.com>
2021-05-28 02:28:38 +0000

    setup PokemonAdd to add new pokemon using services and POST method

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import PokemonsDetail from './pages/pokemon-detail';
 import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PageNotFound from './pages/page-not-found';
 import PokemonEdit from './pages/pokemon-edit';
-
+import PokemonAdd from './pages/pokemon-add';
 
 
 
@@ -23,6 +23,7 @@ const App: FunctionComponent = () => {
             <Switch>
                 <Route exact path="/" component={PokemonList} />
                 <Route exact path="/pokemons" component={PokemonList} />
+                <Route exact path='/pokemon/add' component={PokemonAdd} />
                 <Route exact path="/pokemons/edit/:id" component={PokemonEdit} />
                 <Route path="/pokemons/:id" component={PokemonsDetail} />
                 <Route component={PageNotFound} />
--- src/services/pokemon-services.ts
+++ src/services/pokemon-services.ts
@@ -1,14 +1,14 @@
 import Pokemon from '../models/pokemon';
  
 export default class PokemonService {
- 
+  // Fetch pokemons from from the json-server
   static getPokemons(): Promise<Pokemon[]> {
-
     return fetch('http://localhost:3001/pokemons')
       .then(response => response.json())
       .catch(error => this.handleError(error));
   }
  
+  // Fetch pokemon with the id and handle error
   static getPokemon(id: number): Promise<Pokemon|null> {
     return fetch(`http://localhost:3001/pokemons/${id}`)
       .then(response => response.json())
@@ -17,7 +17,7 @@ export default class PokemonService {
   }
 
 
-
+  // setup update a pokemon and save the data 
   static updatePokemon(pokemon:Pokemon): Promise<Pokemon> {
     return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
       method: 'PUT',
@@ -28,6 +28,7 @@ export default class PokemonService {
     .catch(error => this.handleError(error))
   }
   
+  // setup delete pokemon and save the data  
   static deletePokemon(pokemon: Pokemon): Promise<{}> {
     return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
       method: 'DELETE',
@@ -37,12 +38,25 @@ export default class PokemonService {
     .catch(error => this.handleError(error));
   }
 
+  // add a pokemon 
+  static addPokemon(pokemon: Pokemon): Promise<Pokemon> {
+  //  delete pokemon.created;
 
+   return fetch('http://localhost:3001/pokemons', {
+    method: 'POST',
+    body: JSON.stringify(pokemon),
+    headers: {'Content-Type': 'application/json'}
+   })
+   .then(response => response.json())
+   .catch(error => this.handleError(error));
+  }
+
+  // handle empty data 
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
   }
-
+  // handle error
   static handleError(error: Error):void {
     console.error(error);
   }
-}
+}  
